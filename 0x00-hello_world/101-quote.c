d1fa67ea813bc54bb1185eebd7acde603ab873f0
VOW-07 <omondivictor338@gmail.com>
2022-12-13 03:15:50 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 1 (success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora orpar, 2015-10-19\n", 59);
-	retun(1)
+	write(2, "and that piece of art is useful\" - Dora orpar, 2015-10-19\n", 59);
+	retun(1);
 }
