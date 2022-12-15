4b829f339b753c586ceff2bd82e6855ee60a54bb
VOW-07 <omondivictor338@gmail.com>
2022-12-14 20:49:47 -0800

    vow

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ 
+   * _putchar - writes the character c to stdout
+ 
+    * Return: On Success 1.
+ 
+     * On error return -1
+ 
+      */
+
+int _putchar(char c)
+
+{
+
+	        return (write(1, &c, 1));
+
+}
