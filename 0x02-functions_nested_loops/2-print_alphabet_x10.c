f666c0e7da7dc21c3a6306903f6e7791b6c81ebf
VOW-07 <omondivictor338@gmail.com>
2022-12-14 23:17:38 -0800

    vow

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print a - a ten times
+ *
+ */
+
+void print_alphabet_10(void)
+
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+
+		 _putchar('\n');
+	}
+
+}
